b1395bd866685996069b7165415593ca8133767a
J0020git <ralph.jerome.hernandez@outlook.com>
2024-11-14 20:16:20 +1100

    feat: Dummy saved button

---
+++ components/SavedButton.js
@@ -0,0 +1,30 @@
+import { Pressable, StyleSheet } from "react-native";
+import Ionicons from "@expo/vector-icons/Ionicons";
+
+import styles from "@styles";
+import colours from "@colours";
+
+export default function SavedButton() {
+  return (
+    <Pressable
+      style={savedButton.savedButton}
+      onPress={() => {
+        /* Do something */
+      }}
+    >
+      <Ionicons name="bookmark-outline" size={24} color={colours.darkGrey} />
+    </Pressable>
+  );
+}
+
+const savedButton = StyleSheet.create({
+  savedButton: {
+    height: 32,
+    width: 32,
+    flexDirection: "column",
+    justifyContent: "center",
+    alignItems: "center",
+    backgroundColor: colours.white,
+    borderRadius: 16,
+  },
+});

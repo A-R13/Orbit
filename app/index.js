d6d0b1b55d517d3617f0aacd06e6696ee9083e71
J0020git <ralph.jerome.hernandez@outlook.com>
2024-11-18 23:18:57 +1100

    fix: Load data from AsyncStorage automatically

--- app/index.js
+++ app/index.js
@@ -1,10 +1,5 @@
 import { Redirect } from "expo-router";
-import { useEffect } from "react";
-import dataHandler from "@dataHandler";
 
 export default function IndexScreen() {
-  useEffect(() => {
-    dataHandler.loadData;
-  }, []);
   return <Redirect href="/home" />;
 }
